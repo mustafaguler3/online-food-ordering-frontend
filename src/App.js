b2b7e600a6f7aa3597ee83575980cbdaaee4a37f
mustafa.glr <mustafa.glr@hotmail.com>
2025-08-23 14:24:37 +0300

    add home page component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import RegisterPage from './components/auth/RegisterPage';
 import LoginPage from './components/auth/LoginPage';
+import HomePage from './components/home_menu/HomePage';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Navbar/>
       <div className='content'>
     <Routes>
-      {/* <Route path='/home' element={<Home/>}/> */}
+      {<Route path='/home' element={<HomePage/>}/>}
       {<Route path='/register' element={<RegisterPage/>}/>}
       {<Route path='/login' element={<LoginPage/>}/>}
     </Routes>
